c5c48ac44b6eb17cb5a3893180f943c36b8c6e5b
MaySosa1200 <73956934+MaySosa1200@users.noreply.github.com>
2023-01-28 18:00:19 -0600

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,18 @@
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyAcEiK0E60Rc8I-a_ukay2YqlPkb0u1NoM",
+    authDomain: "rmrbdfx.firebaseapp.com",
+    projectId: "rmrbdfx",
+    storageBucket: "rmrbdfx.appspot.com",
+    messagingSenderId: "18453084868",
+    appId: "1:18453084868:web:3b55c491442e90be6de791",
+    measurementId: "G-HNV5HTQM69"
+});
